6e030691c63f34690da741bea8304a64d1af5406
202207977 <202207977@alu.comillas.edu>
2024-09-30 15:32:34 +0200

    [ADD] Scooter class

---
+++ Scooter.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice1
+{
+    class Scooter : Vehicle
+    {
+        private static string typeOfVehicle = "Scooter";
+
+        public Scooter() : base(typeOfVehicle, null, false) 
+        {
+            Console.WriteLine(WriteMessage("created"));
+        }
+
+        public void Cha()
+        {
+            SetSpeed(100.0f);
+        }
+
+    }
+}
