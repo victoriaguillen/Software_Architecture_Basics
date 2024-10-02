30a91775b48b638e9ca48e7dad8fc698ac56c887
202207977 <202207977@alu.comillas.edu>
2024-10-02 12:33:22 +0200

    [ADD] New type of Vehicles

---
+++ VehicleWithPlate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    abstract class VehicleWithPlate: Vehicle
+    {
+        private string plate;
+
+        public VehicleWithPlate(string typeOfVehicle, string plate) : base(typeOfVehicle)
+        {
+            this.plate = plate;
+        }
+
+        //Override ToString() method with class information
+        public override string ToString()
+        {
+            return $"{GetTypeOfVehicle()} with plate {GetPlate()}";
+        }
+
+
+        public string GetPlate()
+        {
+            return plate;
+        }
+
+    }
+
+
+}
---
+++ VehicleWithoutPlate.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Practice1
+{
+    internal class VehicleWithoutPlate: Vehicle
+    {
+        public VehicleWithoutPlate(string typeOfVehicle) : base(typeOfVehicle)
+        {
+        }
+
+        //Override ToString() method with class information
+        public override string ToString()
+        {
+            return $"{GetTypeOfVehicle()}";
+        }
+    }
+}
